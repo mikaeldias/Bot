fbcc4a7400b17223f0e8d2d6bc19c435ae0a5020
mikaeldias <165174397+MikaelDiasS10@users.noreply.github.com>
2026-04-14 15:20:04 -0300

    news update

--- src/exemples/ex004/ex002.py
+++ src/exemples/ex004/ex002.py
@@ -4,24 +4,23 @@ from langchain_core.messages import BaseMessage
 from typing import Sequence, TypedDict
 from typing_extensions import Annotated
 from rich import print
+from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.graph import END, START, StateGraph, add_messages
 from langgraph.graph.message import Messages
 from langchain.chat_models import init_chat_model
 from rich.markdown import Markdown
+from langgraph.graph.state import RunnableConfig
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_groq import ChatGroq
 
 
 llm = init_chat_model('groq:llama-3.1-8b-instant')
-# 
-def reducer(a: Messages, b: Messages) -> Messages:
-    #print('> reducer', a, b)
-    return add_messages(a, b)
+
 
 
 # defino o state
 class AgentState(TypedDict):
-    messages: Annotated[Sequence[BaseMessage], reducer]
+    messages: Annotated[Sequence[BaseMessage], add_messages]
 
 # defino meus nodes
 def call_llm(state: AgentState) -> AgentState:
@@ -42,12 +41,14 @@ builder.add_edge("__start__", "call_llm")
 builder.add_edge("call_llm", "__end__")
 
 # compilar
-graph = builder.compile()
+checkpointer = InMemorySaver()
+graph = builder.compile(checkpointer=checkpointer)
+config = RunnableConfig(configurable={"thread_id": 1})
 
 # usar o grafo
 
 if __name__ == "__main__":
-    current_messages: Sequence[BaseMessage] = []
+    
     while True:
         user_input = input('Digite para falar com o Gemini: ')
         print(Markdown('---'))
@@ -57,8 +58,9 @@ if __name__ == "__main__":
             print(Markdown('---'))
             break
         human_message = HumanMessage(user_input)
-        current_messages = [*current_messages, human_message]
-        result = graph.invoke({'messages': current_messages})
+        result = graph.invoke(
+         {"messages": [human_message]}
+        )
         current_messages = result['messages']
         print(Markdown(str(result['messages'][-1].content)))
         print(Markdown('---'))
